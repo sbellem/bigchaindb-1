335f18786724ee7dcb8a85ec0749c3ee1efd5a51
Sylvain Bellemare <sylvain@ascribe.io>
2016-12-08 23:15:55 +0100

    try 22

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -7,11 +7,11 @@ pip install --upgrade tox
 
 if [[ ${TOXENV} == py* ]]; then
 
-    if [[ "${TOXENV}" == *-rdb ]]; then
-        sudo apt-get install rethinkdb
-    elif [[ "${TOXENV}" == *-mdb ]]; then
-        sudo apt-get install mongodb-org-server
-    fi
+    #if [[ "${TOXENV}" == *-rdb ]]; then
+    #    sudo apt-get install rethinkdb
+    #elif [[ "${TOXENV}" == *-mdb ]]; then
+    #    sudo apt-get install mongodb-org-server
+    #fi
 
     pip install --upgrade codecov
 fi
