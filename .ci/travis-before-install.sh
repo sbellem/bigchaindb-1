b3fbca2abc6dd39556df6d1d4ee8de5ab42c9737
Sylvain Bellemare <sylvain@ascribe.io>
2016-12-09 03:24:27 +0100

    55

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -14,4 +14,4 @@ fi
 
 wget https://github.com/miloyip/rapidjson/archive/v1.1.0.tar.gz -O /tmp/v1.1.0.tar.gz
 tar -xvf /tmp/v1.1.0.tar.gz
-cp /rapidjson-1.1.0/include/rapidjson /usr/include/
+cp $PWD/rapidjson-1.1.0/include/rapidjson /usr/include/
