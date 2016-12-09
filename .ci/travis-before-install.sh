5605945ad8d07c7d1cdff5a538eb4304daff281e
Sylvain Bellemare <sylvain@ascribe.io>
2016-12-09 01:26:13 +0100

    42

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -11,3 +11,8 @@ elif [[ "${TOXENV}" == *-mdb ]]; then
     echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
     apt-get update -qq
 fi
+
+source /etc/lsb-release
+echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee -a /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+apt-get update -qq
