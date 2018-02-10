b05d33ebac4498f75f9841193c2384d60bf4a961
Sylvain Bellemare <sbellem@gmail.com>
2018-02-11 00:26:47 +0100

    a

--- .ci/travis-after-success.sh
+++ .ci/travis-after-success.sh
@@ -4,10 +4,9 @@ set -e -x
 
 if [[ -z ${TOXENV} ]]; then
     #codecov
-    cd ${TRAVIS_BUILD_DIR}/bigchaindb
+    cd ${TRAVIS_BUILD_DIR}/tests
     ls -l .coverage
     #codecov -v --root ${TRAVIS_BUILD_DIR}/bigchaindb --gcov-root ${TRAVIS_BUILD_DIR}/bigchaindb
     codecov -v
-    ls -al ${TRAVIS_BUILD_DIR}/bigchaindb/coverage.xml
-    ls -al ${TRAVIS_BUILD_DIR}/coverage.xml
+    ls -al ${TRAVIS_BUILD_DIR}/tests/coverage.xml
 fi
