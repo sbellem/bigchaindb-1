e56d13743e5cdae9a70ba40fc86feac200cb0cd4
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 18:56:20 +0100

    a

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [[ -z ${TOXENV} ]]; then
+    sudo apt-get update
+    sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+
     sudo rm /usr/local/bin/docker-compose
     curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > docker-compose
     chmod +x docker-compose
