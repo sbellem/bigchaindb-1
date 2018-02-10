b481668ad4252aaad20e4f66c5f8f456ad2bcdcd
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 05:26:23 +0100

    try

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -6,10 +6,10 @@ if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
     -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
-  docker-compose -f docker-compose.tendermint.yml logs tendermint
-  docker-compose -f docker-compose.tendermint.yml logs bdb
-  sleep 30
-  docker-compose -f docker-compose.tendermint.yml run --rm  --no-deps bdb ping tendermint --timeout 10
+  docker-compose -f docker-compose.tendermint.yml ps
+  sleep 20
+  docker-compose -f docker-compose.tendermint.yml ps
+  docker-compose -f docker-compose.tendermint.yml run --rm  --no-deps bdb ping tendermint --timeout 5
 
   docker-compose -f docker-compose.tendermint.yml up curl-client
   docker-compose -f docker-compose.tendermint.yml logs tendermint
