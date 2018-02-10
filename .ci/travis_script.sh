da40d3bfef298eeaa70efc141bd085957963163a
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 05:11:52 +0100

    try

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -8,11 +8,11 @@ elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
     -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
   docker-compose -f docker-compose.tendermint.yml logs tendermint
   docker-compose -f docker-compose.tendermint.yml logs bdb
-  sleep 20
+  sleep 30
+  docker-compose -f docker-compose.tendermint.yml run --rm  --no-deps bdb ping tendermint --timeout 10
+
   docker-compose -f docker-compose.tendermint.yml up curl-client
   docker-compose -f docker-compose.tendermint.yml logs tendermint
   docker-compose -f docker-compose.tendermint.yml logs bdb
   docker-compose -f docker-compose.tendermint.yml run --rm --no-deps bdb pytest -v --cov=bigchaindb
-  docker-compose -f docker-compose.tendermint.yml logs tendermint
-  docker-compose -f docker-compose.tendermint.yml logs bdb
 fi
