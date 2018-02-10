8755d13197757f67ccfcf046bc27dcb90482e225
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 16:46:30 +0100

    a

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -4,13 +4,13 @@ set -e -x
 
 if [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
         -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
-    docker-compose -f docker-compose.tendermint.yml up -d --no-deps mdb
+    #docker-compose -f docker-compose.tendermint.yml up -d --no-deps mdb
+    #sleep 120
+    #docker-compose -f docker-compose.tendermint.yml up -d --no-deps bdb
+    #sleep 120
+    docker-compose -f docker-compose.tendermint.yml up -d tendermint
     sleep 120
-    docker-compose -f docker-compose.tendermint.yml up -d --no-deps bdb
-    sleep 120
-    docker-compose -f docker-compose.tendermint.yml up -d --no-deps tendermint
-    sleep 60
-    docker-compose -f docker-compose.tendermint.yml logs --tail=20 mdb
-    docker-compose -f docker-compose.tendermint.yml logs --tail=20 bdb
+    docker-compose -f docker-compose.tendermint.yml logs --tail=10 mdb
+    docker-compose -f docker-compose.tendermint.yml logs --tail="all" bdb
     docker-compose -f docker-compose.tendermint.yml logs --tail=20 tendermint
 fi
