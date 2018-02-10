b7a0cf348c638154daf2c5812f5a76b5a73530c4
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 03:22:22 +0100

    try using docker-compose for travis

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -3,17 +3,7 @@
 set -e -x
 
 if [[ -n ${TOXENV} ]]; then
-  tox -e ${TOXENV}
+  docker-compose -f docker-compose.tendermint.yml run --rm --no-deps bdb tox -e ${TOXENV}
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
     -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
-    # Run the full suite of tests for MongoDB over an unsecure connection
-  pytest -sv --database-backend=localmongodb --cov=bigchaindb -m tendermint
-elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
-    "${BIGCHAINDB_DATABASE_SSL}" == true ]]; then
-    # Run a sub-set of tests over SSL; those marked as 'pytest.mark.bdb_ssl'.
-  pytest -sv --database-backend=localmongodb-ssl --cov=bigchaindb -m bdb_ssl
-else
-  # Run the full suite of tests for RethinkDB (the default backend when testing)
-  pytest -sv -m "serial"
-  pytest -sv --cov=bigchaindb -m "not serial"
-fi
+  docker-compose -f docker-compose.tendermint.yml run --rm --no-deps bdb pytest -v --cov=bigchaindb
