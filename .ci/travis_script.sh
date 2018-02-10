f335f8c0cef9eab978cf9add83cba96d39f9d321
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 04:14:47 +0100

    curl client check

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -7,5 +7,6 @@ if [[ -n ${TOXENV} ]]; then
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
     -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
   sleep 20
+  docker-compose -f docker-compose.tendermint.yml up curl-client
   docker-compose -f docker-compose.tendermint.yml run --rm --no-deps bdb pytest -v --cov=bigchaindb
 fi
