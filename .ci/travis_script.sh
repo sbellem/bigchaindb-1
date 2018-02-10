60f98f63b2485c5f849fb2620b7ed3e6ac519133
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 03:58:06 +0100

    sleep 20

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -6,6 +6,6 @@ if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
     -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
-  sleep 5
+  sleep 20
   docker-compose -f docker-compose.tendermint.yml run --rm --no-deps bdb pytest -v --cov=bigchaindb
 fi
