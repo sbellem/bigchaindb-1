d82935a0b60fdc5cd41441e0a1ffea33400683f0
Sylvain Bellemare <sbellem@gmail.com>
2018-02-10 06:14:28 +0100

    d

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -10,6 +10,7 @@ if [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
     sleep 20
     docker-compose -f docker-compose.tendermint.yml up -d --no-deps tendermint
     sleep 20
+    docker-compose -f docker-compose.tendermint.yml logs --tail=20 mdb
     docker-compose -f docker-compose.tendermint.yml logs --tail=20 bdb
     docker-compose -f docker-compose.tendermint.yml logs --tail=20 tendermint
 fi
